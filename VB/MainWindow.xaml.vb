6efca6d90a74efc52409cbe223be912a7489eca8
DevExpressExampleBot <fakeEmail>
2021-12-17 18:11:06 +0300

    autogenerate VB

--- VB/MainWindow.xaml.vb
+++ VB/MainWindow.xaml.vb
@@ -1,40 +1,32 @@
-﻿Imports Microsoft.VisualBasic
 Imports System
 Imports System.Windows
 Imports System.Windows.Threading
 Imports Model
 
 Namespace DXGridThreads
-	''' <summary>
-	''' Interaction logic for MainWindow.xaml
-	''' </summary>
-	Partial Public Class MainWindow
-		Inherits Window
-		Private viewModel As ViewModel
 
-		Public Sub New()
-			InitializeComponent()
-			viewModel = New ViewModel()
-			AddHandler viewModel.AsyncProcessingStarted, AddressOf viewModel_AsyncProcessingStarted
-			AddHandler viewModel.AsyncProcessingCompleted, AddressOf viewModel_AsyncProcessingCompleted
-			DataContext = viewModel
-		End Sub
+    ''' <summary>
+    ''' Interaction logic for MainWindow.xaml
+    ''' </summary>
+    Public Partial Class MainWindow
+        Inherits Window
 
-		Private Sub viewModel_AsyncProcessingStarted(ByVal sender As Object, ByVal e As EventArgs)
-			grid.Dispatcher.Invoke(New Action(Function() AnonymousMethod1()), DispatcherPriority.DataBind)
-		End Sub
-		
-		Private Function AnonymousMethod1() As Boolean
-			grid.BeginDataUpdate()
-			Return True
-		End Function
-		Private Sub viewModel_AsyncProcessingCompleted(ByVal sender As Object, ByVal e As EventArgs)
-			grid.Dispatcher.Invoke(New Action(Function() AnonymousMethod2()), DispatcherPriority.DataBind)
-		End Sub
-		
-		Private Function AnonymousMethod2() As Boolean
-			grid.EndDataUpdate()
-			Return True
-		End Function
-	End Class
+        Private viewModel As ViewModel
+
+        Public Sub New()
+            Me.InitializeComponent()
+            viewModel = New ViewModel()
+            AddHandler viewModel.AsyncProcessingStarted, New EventHandler(AddressOf viewModel_AsyncProcessingStarted)
+            AddHandler viewModel.AsyncProcessingCompleted, New EventHandler(AddressOf viewModel_AsyncProcessingCompleted)
+            DataContext = viewModel
+        End Sub
+
+        Private Sub viewModel_AsyncProcessingStarted(ByVal sender As Object, ByVal e As EventArgs)
+            Me.grid.Dispatcher.Invoke(New Action(Sub() Me.grid.BeginDataUpdate()), DispatcherPriority.DataBind)
+        End Sub
+
+        Private Sub viewModel_AsyncProcessingCompleted(ByVal sender As Object, ByVal e As EventArgs)
+            Me.grid.Dispatcher.Invoke(New Action(Sub() Me.grid.EndDataUpdate()), DispatcherPriority.DataBind)
+        End Sub
+    End Class
 End Namespace
--- VB/ViewModel.vb
+++ VB/ViewModel.vb
@@ -1,109 +1,127 @@
-﻿Imports Microsoft.VisualBasic
 Imports System
 Imports System.Collections.ObjectModel
 Imports System.ComponentModel
-Imports System.Linq
 Imports System.Threading
 
 Namespace Model
-	Public Class ViewModel
-		Implements INotifyPropertyChanged
-		Private Shared SyncRoot As Object = New Object()
-		Private timer As Timer
-		Private random As New Random()
 
-		Private Sub TimerCallback(ByVal state As Object)
-			SyncLock SyncRoot
-				OnAsyncProcessingStarted()
-				For Each item As DataItem In Source
-					item.Value = random.Next(100)
-				Next item
-				OnAsyncProcessingCompleted()
-			End SyncLock
-		End Sub
+    Public Class ViewModel
+        Implements INotifyPropertyChanged
 
-		Public Sub New()
-			source_Renamed = DataItem.Data
-			timer = New Timer(AddressOf TimerCallback, Nothing, 1000, 1000)
-		End Sub
+        Private Shared SyncRoot As Object = New Object()
 
-		Private source_Renamed As ObservableCollection(Of DataItem)
-		Public Property Source() As ObservableCollection(Of DataItem)
-			Get
-				Return source_Renamed
-			End Get
-			Set(ByVal value As ObservableCollection(Of DataItem))
-				If source_Renamed Is value Then
-					Return
-				End If
-				source_Renamed = value
-				OnPropertyChanged("Source")
-			End Set
-		End Property
+        Private timer As Timer
 
-		Public Event AsyncProcessingStarted As EventHandler
-		Public Event AsyncProcessingCompleted As EventHandler
+        Private random As Random = New Random()
 
-		Private Sub OnAsyncProcessingStarted()
-			RaiseEvent AsyncProcessingStarted(Me, EventArgs.Empty)
-		End Sub
-		Private Sub OnAsyncProcessingCompleted()
-			RaiseEvent AsyncProcessingCompleted(Me, EventArgs.Empty)
-		End Sub
+        Private Sub TimerCallback(ByVal state As Object)
+            SyncLock SyncRoot
+                OnAsyncProcessingStarted()
+                For Each item As DataItem In Source
+                    item.Value = random.Next(100)
+                Next
 
-		#Region "INotifyPropertyChanged members"
-		Public Event PropertyChanged As PropertyChangedEventHandler Implements INotifyPropertyChanged.PropertyChanged
+                OnAsyncProcessingCompleted()
+            End SyncLock
+        End Sub
 
-		Private Sub OnPropertyChanged(ByVal propertyName As String)
-			RaiseEvent PropertyChanged(Me, New PropertyChangedEventArgs(propertyName))
-		End Sub
-		#End Region
-	End Class
+        Public Sub New()
+            sourceField = DataItem.Data
+            timer = New Timer(AddressOf TimerCallback, Nothing, 1000, 1000)
+        End Sub
 
-	Public Class DataItem
-		Implements INotifyPropertyChanged
-		Private Shared data_Renamed As ObservableCollection(Of DataItem)
-		Public Shared ReadOnly Property Data() As ObservableCollection(Of DataItem)
-			Get
-				If data_Renamed Is Nothing Then
-					data_Renamed = New ObservableCollection(Of DataItem)()
-					For i As Integer = 0 To 99
-						data_Renamed.Add(New DataItem() With {.Name = "Name" & i.ToString(), .Value = i})
-					Next i
-				End If
-				Return data_Renamed
-			End Get
-		End Property
+        Private sourceField As ObservableCollection(Of DataItem)
 
-		Private privateName As String
-		Public Property Name() As String
-			Get
-				Return privateName
-			End Get
-			Set(ByVal value As String)
-				privateName = value
-			End Set
-		End Property
-		Private valueField As Integer
-		Public Property Value() As Integer
-			Get
-				Return valueField
-			End Get
-			Set(ByVal value As Integer)
-				valueField = value
-				OnPropertyChanged("Value")
-			End Set
-		End Property
-		Public Overrides Function ToString() As String
-			Return Name
-		End Function
+        Public Property Source As ObservableCollection(Of DataItem)
+            Get
+                Return sourceField
+            End Get
 
-		#Region "INotifyPropertyChanged members"
-		Public Event PropertyChanged As PropertyChangedEventHandler Implements INotifyPropertyChanged.PropertyChanged
+            Set(ByVal value As ObservableCollection(Of DataItem))
+                If sourceField Is value Then Return
+                sourceField = value
+                OnPropertyChanged("Source")
+            End Set
+        End Property
 
-		Private Sub OnPropertyChanged(ByVal propertyName As String)
-			RaiseEvent PropertyChanged(Me, New PropertyChangedEventArgs(propertyName))
-		End Sub
-		#End Region
-	End Class
+        Public Event AsyncProcessingStarted As EventHandler
+
+        Public Event AsyncProcessingCompleted As EventHandler
+
+        Private Sub OnAsyncProcessingStarted()
+            RaiseEvent AsyncProcessingStarted(Me, EventArgs.Empty)
+        End Sub
+
+        Private Sub OnAsyncProcessingCompleted()
+            RaiseEvent AsyncProcessingCompleted(Me, EventArgs.Empty)
+        End Sub
+
+'#Region "INotifyPropertyChanged members"
+        Public Event PropertyChanged As PropertyChangedEventHandler Implements INotifyPropertyChanged.PropertyChanged
+
+        Private Sub OnPropertyChanged(ByVal propertyName As String)
+            RaiseEvent PropertyChanged(Me, New PropertyChangedEventArgs(propertyName))
+        End Sub
+'#End Region
+    End Class
+
+    Public Class DataItem
+        Implements INotifyPropertyChanged
+
+        Private Shared dataField As ObservableCollection(Of DataItem)
+
+        Public Shared ReadOnly Property Data As ObservableCollection(Of DataItem)
+            Get
+                If dataField Is Nothing Then
+                    dataField = New ObservableCollection(Of DataItem)()
+                    For i As Integer = 0 To 100 - 1
+                        Call dataField.Add(New DataItem() With {.Name = "Name" & i.ToString(), .Value = i})
+                    Next
+                End If
+
+                Return dataField
+            End Get
+        End Property
+
+        Public Property Name As String
+
+         ''' Cannot convert FieldDeclarationSyntax, System.NotSupportedException: VolatileKeyword is not supported!
+'''    at ICSharpCode.CodeConverter.VB.SyntaxKindExtensions.ConvertToken(SyntaxKind t, TokenContext context)
+'''    at ICSharpCode.CodeConverter.VB.CommonConversions.ConvertModifier(SyntaxToken m, TokenContext context)
+'''    at ICSharpCode.CodeConverter.VB.CommonConversions.<>c__DisplayClass30_0.<ConvertModifiersCore>b__3(SyntaxToken x)
+'''    at System.Linq.Enumerable.WhereSelectEnumerableIterator`2.MoveNext()
+'''    at System.Linq.Enumerable.WhereSelectEnumerableIterator`2.MoveNext()
+'''    at System.Collections.Generic.List`1..ctor(IEnumerable`1 collection)
+'''    at System.Linq.Enumerable.ToList[TSource](IEnumerable`1 source)
+'''    at ICSharpCode.CodeConverter.VB.CommonConversions.ConvertModifiersCore(IReadOnlyCollection`1 modifiers, TokenContext context, Boolean isConstructor, Boolean isNestedType)
+'''    at ICSharpCode.CodeConverter.VB.NodesVisitor.VisitFieldDeclaration(FieldDeclarationSyntax node)
+'''    at Microsoft.CodeAnalysis.CSharp.CSharpSyntaxVisitor`1.Visit(SyntaxNode node)
+'''    at ICSharpCode.CodeConverter.VB.CommentConvertingVisitorWrapper`1.Accept(SyntaxNode csNode, Boolean addSourceMapping)
+''' 
+''' Input:
+'''         volatile int valueField;
+''' 
+'''  Public Property Value As Integer
+            Get
+                Return Me.valueField
+            End Get
+
+            Set(ByVal value As Integer)
+                Me.valueField = value
+                OnPropertyChanged("Value")
+            End Set
+        End Property
+
+        Public Overrides Function ToString() As String
+            Return Name
+        End Function
+
+'#Region "INotifyPropertyChanged members"
+        Public Event PropertyChanged As PropertyChangedEventHandler Implements INotifyPropertyChanged.PropertyChanged
+
+        Private Sub OnPropertyChanged(ByVal propertyName As String)
+            RaiseEvent PropertyChanged(Me, New PropertyChangedEventArgs(propertyName))
+        End Sub
+'#End Region
+    End Class
 End Namespace
